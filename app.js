594d864b04ecdba745170a67d954bb9585bb25e1
Adam Purcell <apurcell88@gmail.com>
2025-05-19 14:29:50 -0500

    Set up express config in app.js.

--- app.js
+++ app.js
@@ -1,5 +1,58 @@
 require("dotenv").config();
+
 const express = require("express");
 const session = require("express-session");
+const pgSession = require("connect-pg-simple")(session);
 const passport = require("passport");
-const pool = require("./config/database");
+const path = require("path");
+
+// Custom imports
+const pool = require("./config/pool");
+require("./config/passport");
+const routes = require("./routes");
+
+// Initialize Express
+const app = express();
+
+// ------------------- MIDDLEWARE -------------------
+
+// Body parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static assets
+app.use(express.static(path.join(__dirname, "public")));
+
+// Template engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+//------------------- SESSION SETUP -------------------
+app.use(
+  session({
+    store: new pgSession({
+      pool,
+      tableName: "session",
+    }),
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      mageAge: 1000 * 60 * 60 * 24, // 1 day
+      secure: false, // set true if using HTTPS in production
+    },
+  })
+);
+
+// ------------------- PASSPORT SETUP -------------------
+app.use(passport.initialize());
+app.use(passport.session());
+
+// ------------------- ROUTES -------------------
+app.use(routes); // routes will use controller functions
+
+// ------------------- SERVER START -------------------
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
